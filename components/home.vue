2b0d190fd1a5e6b5645707dc9115995004d51b92
SuryaWilliam <eswuis@gmail.com>
2022-11-17 18:31:15 +0700

    asdasdasd

---
+++ components/home.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <header class="relative flex items-center justify-center h-screen overflow-hidden">
+    <video autoplay loop muted playsinline class="fixed z-10 w-full ">
+      <source src="/bg.mp4" type="video/mp4" />
+    </video>
+    <video autoplay loop muted playsinline class="fixed z-10 w-full min-[500px]:hidden">
+      <source src="/bgm.mp4" type="video/mp4" />
+    </video>
+
+
+  </header>
+
+</div>
+</template>
+
+<script>
+export default {
+name:'home'
+}
+</script>
+
+<style>
+
+</style>
