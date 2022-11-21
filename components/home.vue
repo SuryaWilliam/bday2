4fe4400c71b4c0b9976d9d115d6bb3bb8b785378
SuryaWilliam <eswuis@gmail.com>
2022-11-21 15:51:15 +0700

    asdasdasd

--- components/home.vue
+++ components/home.vue
@@ -98,8 +98,9 @@ var ameibday = setInterval(function () {
 
 <style>
 #myb {
-  text-shadow: 0 0 1px rgb(255, 210, 108), 0 0 5px rgb(255, 210, 108),
-    0 0 10px rgb(255, 210, 108), 0 0 15px #000, 0 0 20px #000;
+  text-shadow: 0 0 1px 0px rgba(255, 210, 108, 1),
+    0 0 5px 0px rgba(255, 210, 108, 1), 0 0 10px 0px rgba(255, 210, 108, 1),
+    0 0 15px 0px #000, 0 0 20px 0px #000;
   font-weight: bold;
 }
 
